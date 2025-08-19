e175bde82eba45d811fb97acaaad1fe331e61974
JYOTHISH1823 <jyothish1823@gmail.com>
2025-08-19 17:44:53 +0530

    Initial commit: 8-bit Register File project

---
+++ run_icarus.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Compiling Register File...
+iverilog -o register_file_tb.out ..\src\register_file.v ..\tb\register_file_tb.v
+
+echo Running Simulation...
+vvp register_file_tb.out
+
+echo Opening Waveform in GTKWave...
+gtkwave register_file.vcd
